da61f3cb73d51bc7eb93c1d1639ed4bd662575d4
Jakob Persons <jakobep24@gmail.com>
2021-01-11 07:51:10 -0500

    test passing

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31
+const num2 = 2
 
-// Write your code here
+const multiply = (num1 * num2)
+
+const random = Math.floor(Math.random() * 10)
+
+const mod = 10 % 6
+
+const max = Math.max(2, 4, 6, 8, 10, 15, 20)
